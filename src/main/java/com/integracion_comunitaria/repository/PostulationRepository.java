6e671437ff00f2efc350aa5e0f0b276a2380372c
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-04-21 12:47:22 -0300

    feat(repository): agrega metodo findByPetition Se agrego el metodo findByPetition al repositorio PostulationRepository

--- src/main/java/com/integracion_comunitaria/repository/PostulationRepository.java
+++ src/main/java/com/integracion_comunitaria/repository/PostulationRepository.java
@@ -1,9 +1,13 @@
 package com.integracion_comunitaria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.integracion_comunitaria.model.Postulation;
 
 
 public interface PostulationRepository extends JpaRepository<Postulation, Long> {
+
+    List<Postulation> findByPetition(Long idPetition);
 
 }
